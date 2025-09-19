ea958fda4134a75e1fb1bb1adcf93b455dab4cf0
AKBAR U <91365919+urlenbayev@users.noreply.github.com>
2025-09-20 02:53:53 +0300

    5.14

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,17 +1,19 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog.jsx";
-import { expect } from "vitest";
+import { expect, test } from "vitest";
+
+const blog = {
+  id: "1",
+  title: "Test Title",
+  author: "Test Author",
+  url: "http://example.com",
+  likes: 5,
+  user: { username: "testuser", name: "bruh", token: "wqeww121" },
+};
 
 // eslint-disable-next-line no-undef
 test("render author and title, but not url and likes", () => {
-  const blog = {
-    id: "1",
-    title: "Test Title",
-    author: "Test Author",
-    url: "http://example.com",
-    likes: 5,
-    user: { username: "testuser", name: "bruh", token: "wqeww121" },
-  };
   render(
     <Blog
       blog={blog}
@@ -29,4 +31,25 @@ test("render author and title, but not url and likes", () => {
   expect(author).toBeDefined();
   expect(url).not.toBeVisible();
   expect(likes).not.toBeVisible();
+});
+
+test.only("url and likes are visible when toggle button pressed", async () => {
+  render(
+    <Blog
+      blog={blog}
+      handleDelete={() => {}}
+      handleLike={() => {}}
+      user={blog.user}
+    />
+  );
+  const viewButton = screen.getByText("View");
+  await userEvent.click(viewButton);
+  const url = screen.getByText("Link: http://example.com");
+  const likes = screen.getByText("likes 5");
+
+  screen.debug(url);
+  screen.debug(likes);
+
+  expect(url).toBeVisible();
+  expect(likes).toBeVisible();
 });
