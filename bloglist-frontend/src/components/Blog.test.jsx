6751bf22f45e0216fbc98a7957ffa73374cddf62
AKBAR U <91365919+urlenbayev@users.noreply.github.com>
2025-09-20 03:22:24 +0300

    5.15

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Blog from "./Blog.jsx";
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 
 const blog = {
   id: "1",
@@ -33,7 +33,7 @@ test("render author and title, but not url and likes", () => {
   expect(likes).not.toBeVisible();
 });
 
-test.only("url and likes are visible when toggle button pressed", async () => {
+test("url and likes are visible when toggle button pressed", async () => {
   render(
     <Blog
       blog={blog}
@@ -52,4 +52,24 @@ test.only("url and likes are visible when toggle button pressed", async () => {
 
   expect(url).toBeVisible();
   expect(likes).toBeVisible();
+});
+
+test.only("event handler for like called twice when like pressed twice", async () => {
+  const mockHandler = vi.fn();
+  render(
+    <Blog
+      blog={blog}
+      handleDelete={() => {}}
+      handleLike={mockHandler}
+      user={blog.user}
+    />
+  );
+
+  const viewButton = screen.getByText("View");
+  await userEvent.click(viewButton);
+  const likeButton = screen.getByText("like");
+  await userEvent.click(likeButton);
+  await userEvent.click(likeButton);
+
+  expect(mockHandler.mock.calls).toHaveLength(2);
 });
